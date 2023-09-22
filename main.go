f450ee9cc26f6e1caee554f8632ff33d73889306
seanomeara96 <seanom96@gmail.com>
2023-09-22 16:40:13 +0100

    add more descriptive logs

--- main.go
+++ main.go
@@ -76,7 +76,7 @@ func main() {
 	r.HandleFunc("/manage-subscription", handler.CreateCustomerPortalSession).Methods(http.MethodGet)
 	r.HandleFunc("/webhook", handler.HandleStripeWebhook).Methods(http.MethodPost)
 
-	log.Println("http://localhost:3000")
+	log.Println("Server started. Listening on http://localhost:3000")
 	log.Fatal(http.ListenAndServe(":3000", r))
 	//log.Fatal(http.ListenAndServeTLS(":3000", "localhost.crt", "localhost.key", r))
 }
