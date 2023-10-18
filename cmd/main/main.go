75ca6d6791d9417ad3b5ec9fef6f6d9e112d6e4c
Sean O'Meara <49831134+seanomeara96@users.noreply.github.com>
2023-10-18 11:59:52 +0100

    moved routes to routes.go

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -129,23 +129,7 @@ func main() {
 
 	fs := http.FileServer(http.Dir("assets"))
 
-	r := mux.NewRouter()
-	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", fs))
-	r.HandleFunc("/", handler.HomePage).Methods(http.MethodGet)
-	r.HandleFunc("/signup", handler.SignupPage).Methods(http.MethodGet)
-	r.HandleFunc("/signup", handler.CreateUser).Methods(http.MethodPost)
-	r.HandleFunc("/success", handler.SuccessPage).Methods(http.MethodGet)
-	r.HandleFunc("/cancel", handler.CancelPage).Methods(http.MethodGet)
-	r.HandleFunc("/upgrade", handler.UpgradePage).Methods(http.MethodGet)
-	r.HandleFunc("/login", handler.Login).Methods(http.MethodPost)
-	r.HandleFunc("/logout", handler.Logout).Methods(http.MethodGet)
-	r.HandleFunc("/todo/add", handler.AddTodo).Methods(http.MethodPost)
-	r.HandleFunc("/todo/update/description", handler.UpdateTodoDescription).Methods(http.MethodPost)
-	r.HandleFunc("/todo/update/status/{id}", handler.UpdateTodoStatus).Methods(http.MethodPost)
-	r.HandleFunc("/todo/remove/{id}", handler.RemoveTodo).Methods(http.MethodPost)
-	r.HandleFunc("/create-checkout-session", handler.CreateCheckoutSession).Methods(http.MethodPost)
-	r.HandleFunc("/manage-subscription", handler.CreateCustomerPortalSession).Methods(http.MethodGet)
-	r.HandleFunc("/webhook", handler.HandleStripeWebhook).Methods(http.MethodPost)
+	r := routes.Router(handler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
